62a36aa92bcd9878354cac2f9311ac0e0f750dee
fernandeslouro <jd.louro@outlook.pt>
2021-09-17 11:11:13 +0100

    clear unnecessary vars

--- tmux_time.sh
+++ tmux_time.sh
@@ -4,51 +4,38 @@ PID_FILE=/tmp/stopwatch_pid
 TIME_FILE=/tmp/stopwatch
 
 Time_fn () {
-  booly="$1"
   if test -f "$TIME_FILE"; then
       timestr=$(head -n 1 $TIME_FILE)
-      if [ "$booly" = "true" ]; then
+      if [ "$1" = "true" ]; then
         rm -f $TIME_FILE 
       fi
   fi
 }
 
 Pid () {
-  booly="$1"
   if test -f "$PID_FILE"; then
       pidstr=$(head -n 1 $PID_FILE)
-      if [ "$booly" = "true" ]; then
+      if [ "$1" = "true" ]; then
         rm -f $PID_FILE
       fi
   fi
 }
+
 order="$1"
 time="$2"
+
 if [ "$order" = "start" ]; then
   Pid true
   Time_fn true
-  printf "$time"
-  printf "|"
-  printf "\n"
   sh -c 'echo $$ > /tmp/stopwatch_pid; exec termdown '"${time}"' -o /tmp/stopwatch > /dev/null' 
-  #termdown ${time} -o /tmp/stopwatch > /dev/null 
 
-  #counter_pid=$!
-  #printf "$counter_pid"
-  #echo "$counter_pid" >> "$PID_FILE"
-
-  printf "aystret"
-  printf "\n"
- 
 elif [ "$order" = "pause" ]; then
   Pid false
-  printf "aystret"
-  printf "$pidstr"
   kill $pidstr
+
 elif [ "$order" = "continue" ];then
   Pid true
   Time_fn true
-  printf "$timestr"
   sh -c 'echo $$ > /tmp/stopwatch_pid; exec termdown '"${timestr// /}"' -o /tmp/stopwatch > /dev/null' 
 fi
 
